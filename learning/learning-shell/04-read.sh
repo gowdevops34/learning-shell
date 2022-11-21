9f800eb7cd414411d92941b95373f002d36aa3fa
gowdevops34 <kgowthami34@gmail.com>
2022-11-21 22:44:40 +0530

    read command

---
+++ learning/learning-shell/04-read.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+read -p 'Enter your name: ' name
+echo -e "Hey $name, Welcome to Devops Training......"
+
+read -p 'Enter your phone number: ' phone
+echo "Thanks for entering details, Will text you details soon to $phone"
