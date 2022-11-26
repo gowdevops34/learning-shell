b6079fdf64f948aeb49a57d338b45f5934d05d49
gowdevops34 <kgowthami34@gmail.com>
2022-11-26 15:09:13 +0530

    inputs

--- learning/learning-shell/05-special-vars.sh
+++ learning/learning-shell/05-special-vars.sh
@@ -5,3 +5,5 @@ echo First Input = $1
 echo Second Input = $2
 echo All Inputs = $*
 echo Number of Inputs = $#
+
+#from 10th value we need toacess the variable as ${10} instead of $10,because without braces it consider as $1
